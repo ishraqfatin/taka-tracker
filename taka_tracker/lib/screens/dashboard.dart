fc8c24e50df04b168c2c00bdfb43c997790f541a
shihab <shihab.hasan3322@gmail.com>
2024-04-27 16:51:37 +0600

    feat: add bottom navbar with scrollable expense list

--- taka_tracker/lib/screens/dashboard.dart
+++ taka_tracker/lib/screens/dashboard.dart
@@ -60,82 +60,90 @@ class _DashboardScreenState extends State<StatefulWidget> {
           )
         ],
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 250,
-              child: CustomLineChart(),
-            ),
-            const Text("\nExpenses"),
-            StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(currentUser?.uid)
-                  .collection('expenses')
-                  .snapshots(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  final snap = snapshot.data!.docs;
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    primary: false,
-                    itemCount: snap.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        height: 70,
-                        width: double.infinity,
-                        margin: const EdgeInsets.only(bottom: 12),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black26,
-                              offset: Offset(2, 2),
-                              blurRadius: 10,
-                            ),
-                          ],
-                        ),
-                        child: Stack(
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.only(left: 20),
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                snap[index]['name'],
-                                style: const TextStyle(
-                                  color: Colors.black54,
-                                  fontWeight: FontWeight.bold,
-                                ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 250,
+            child: CustomLineChart(),
+          ),
+          const Text("\nExpenses"),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(currentUser?.uid)
+                        .collection('expenses')
+                        .snapshots(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData) {
+                        final snap = snapshot.data!.docs;
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          primary: false,
+                          itemCount: snap.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              height: 70,
+                              width: double.infinity,
+                              margin: const EdgeInsets.only(bottom: 12),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.black26,
+                                    offset: Offset(2, 2),
+                                    blurRadius: 10,
+                                  ),
+                                ],
                               ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(right: 20),
-                              alignment: Alignment.centerRight,
-                              child: Text(
-                                "\$${snap[index]['price']}",
-                                style: TextStyle(
-                                  color: Colors.green.withOpacity(0.7),
-                                  fontWeight: FontWeight.bold,
-                                ),
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.only(left: 20),
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      snap[index]['name'],
+                                      style: const TextStyle(
+                                        color: Colors.black54,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: const EdgeInsets.only(right: 20),
+                                    alignment: Alignment.centerRight,
+                                    child: Text(
+                                      "\$${snap[index]['price']}",
+                                      style: TextStyle(
+                                        color: Colors.green.withOpacity(0.7),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
-                        ),
-                      );
+                            );
+                          },
+                        );
+                      } else {
+                        return const SizedBox();
+                      }
                     },
-                  );
-                } else {
-                  return const SizedBox();
-                }
-              },
-            )
-          ],
-        ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
+        shape: CircleBorder(),
         onPressed: () {
           // Show the popup screen
           showModalBottomSheet(
@@ -147,6 +155,22 @@ class _DashboardScreenState extends State<StatefulWidget> {
         child: const Icon(
           Icons.add,
           color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+            Opacity(
+                opacity: 0.0,
+                child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons
+                        .menu))), // I want this icon button to be invisible and unclickable
+            IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+          ],
         ),
       ),
     );
